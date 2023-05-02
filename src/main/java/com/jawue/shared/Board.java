f792644925d059061b03869e01d75259a5b91330
jan_wue <jan_wue@proton.me>
2023-05-02 16:10:53 +0200

    client can now print the game board

--- src/main/java/com/jawue/shared/Board.java
+++ src/main/java/com/jawue/shared/Board.java
@@ -12,7 +12,34 @@ public class Board {
   public String toString() {
     return Arrays.deepToString(board);
   }
+
+
+
   public void print() {
-    this.toString();
+    System.out.println("       |     |");
+    for (int i = 0; i < 3; i++) {
+
+      String newLine = "";
+
+      System.out.print(i + " ");
+
+      for (int j = 0; j < 3; j++) {
+        newLine = newLine + "  " + (this.board[i][j]);
+
+        if (j != 2) {
+
+          newLine = newLine + "  |";
+        }
+      }
+      System.out.println(newLine);
+
+      if (i != 2) {
+
+        System.out.println("  -----|-----|-----    ");
+      }
+    }
+
+    System.out.println("       |     |");
+    System.out.println("\n   " + " A " + "    B " + "    C  ");
   }
 }
